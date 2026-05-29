92fd3e0ef0de90867a5d591d843d1c62958a914e
inlisboa-coder <inlisboa@gmail.com>
2026-05-29 21:22:36 +0100

    v16.0 Portugal adicionar pintura

---
+++ src/portugal/pintura.ts
@@ -0,0 +1,8 @@
+export default {
+  area: 'Pintura',
+  pais: 'Portugal',
+  artistasTopActual: ['Vhils','Joana Vasconcelos','Pedro Cabrita Reis','Graça Morais','Pedro Calapez','Jorge Martins','Ana Vidigal','José Loureiro','Miguel Branco','João Queiroz'],
+  artistasMelhoresSempre: ['Nuno Gonçalves','Grão Vasco','Domingos Sequeira','José Malhoa','Columbano Bordalo Pinheiro','Amadeo de Souza-Cardoso','Almada Negreiros','Vieira da Silva','Júlio Pomar','Paula Rego'],
+  obrasActuais: ['Intervenções urbanas de Vhils','Instalações de Joana Vasconcelos','Séries recentes de Pedro Cabrita Reis','Pintura contemporânea de Graça Morais','Obras recentes de Pedro Calapez','Trabalhos recentes de Jorge Martins','Colagens de Ana Vidigal','Pintura de José Loureiro','Obras de Miguel Branco','Paisagens de João Queiroz'],
+  melhoresObrasSempre: ['Painéis de São Vicente','O Fado','Os Emigrantes','Lusitânia no Bairro Latino','Retrato de Fernando Pessoa','Meninas de Paula Rego','Composição de Vieira da Silva','Auto-retrato de Amadeo','Pintura de Júlio Pomar','Obras de Grão Vasco']
+};
