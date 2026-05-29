7b5225434898a6f335aa6afd6648fbb6d3e15cdb
inlisboa-coder <inlisboa@gmail.com>
2026-05-29 21:24:45 +0100

    v16.0 Portugal adicionar escultura

---
+++ src/portugal/escultura.ts
@@ -0,0 +1,8 @@
+export default {
+  area: 'Escultura',
+  pais: 'Portugal',
+  artistasTopActual: ['Rui Chafes','Joana Vasconcelos','José Pedro Croft','Pedro Cabrita Reis','Francisco Tropa','Fernanda Fragateiro','Ângela Ferreira','Miguel Palma','Pedro Tudela','Paulo Neves'],
+  artistasMelhoresSempre: ['Machado de Castro','Soares dos Reis','Canto da Maia','João Cutileiro','Lagoa Henriques','António Duarte','Barata Feyo','Leopoldo de Almeida','Jorge Vieira','Alberto Carneiro'],
+  obrasActuais: ['Esculturas de Rui Chafes','Instalações de Joana Vasconcelos','Obras de José Pedro Croft','Intervenções de Cabrita Reis','Trabalhos de Francisco Tropa','Obras de Fernanda Fragateiro','Instalações de Ângela Ferreira','Peças de Miguel Palma','Escultura sonora de Pedro Tudela','Escultura em madeira de Paulo Neves'],
+  melhoresObrasSempre: ['O Desterrado','Monumento ao Marquês de Pombal','Cristo Rei','Fernando Pessoa de Lagoa Henriques','Obras de Cutileiro','Escultura de Soares dos Reis','Obras de Machado de Castro','Escultura pública portuguesa','Monumentos nacionais','Obras de Alberto Carneiro']
+};
